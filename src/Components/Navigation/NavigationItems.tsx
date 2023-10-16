c1e9245b5c5aa6603848e525528909416fc8a283
Felicia Brenner <felicia.brenner514@gmail.com>
2023-10-15 21:27:27 -0400

    fix: remove the LinkWrapper from Navigation component

---
+++ src/Components/Navigation/NavigationItems.tsx
@@ -0,0 +1,8 @@
+import LinkWrapper from "../LinkWrapper";
+import styled from "@emotion/styled";
+
+const NavigationItems = styled(LinkWrapper)`
+    font-size: 80px;
+`
+
+export {NavigationItems};
--- src/Components/Navigation/index.tsx
+++ src/Components/Navigation/index.tsx
@@ -1,18 +1,18 @@
 import React from "react"
 import NavigationStyled from "./NaviagationStyled"
-import LinkWrapper from "../LinkWrapper"
 import { CompanyName } from "./CompanyName"
+import {NavigationItems} from './NavigationItems';
 
 function Navigation() {
     return (
         <div>
             <NavigationStyled data-testid="site navigation">
                 <CompanyName to={"../Home"} target={"_blank"}>the skin studio by nikki</CompanyName>
-                <LinkWrapper to={"../About"} target={"_blank"}>About</LinkWrapper>
-                <LinkWrapper to={"../Gallery"} target={"_blank"}>Gallery</LinkWrapper>
-                <LinkWrapper to={"../Details"} target={"_blank"}>Details</LinkWrapper>
-                <LinkWrapper to={"../Testimonials"} target={"_blank"}>Testimonials</LinkWrapper>
-                <LinkWrapper to={"../Contact"} target={"_blank"}>Contact</LinkWrapper>
+                <NavigationItems to={"../About"} target={"_blank"}>About</NavigationItems>
+                <NavigationItems to={"../Gallery"} target={"_blank"}>Gallery</NavigationItems>
+                <NavigationItems to={"../Details"} target={"_blank"}>Details</NavigationItems>
+                <NavigationItems to={"../Testimonials"} target={"_blank"}>Testimonials</NavigationItems>
+                <NavigationItems to={"../Contact"} target={"_blank"}>Contact</NavigationItems>
             </NavigationStyled>
         </div>
     )
